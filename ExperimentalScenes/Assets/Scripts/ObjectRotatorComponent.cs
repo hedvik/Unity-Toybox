03745da4adbfb376df915c5c33ce11452a1c9aa7
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2018-06-12 11:48:32 +0200

    Added a fairly simple scene for experimentation with light mapping and light/reflection probes

---
+++ ExperimentalScenes/Assets/Scripts/ObjectRotatorComponent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class ObjectRotatorComponent : MonoBehaviour {
+    public float speed;
+    public Vector3 axis;
+
+    [HideInInspector] public float angle = 0;
+}
+
+public class ObjectRotatorSystem : ComponentSystem
+{
+    // The components we want to work with in the system
+    private struct SystemComponents
+    {
+        public ObjectRotatorComponent rotator;
+        public Transform transform;
+    }
+
+    protected override void OnUpdate()
+    {
+        foreach(var entity in GetEntities<SystemComponents>())
+        {
+            entity.rotator.angle += Time.deltaTime * entity.rotator.speed;
+            entity.transform.rotation = Quaternion.AngleAxis(entity.rotator.angle, entity.rotator.axis);
+        }
+    }
+}
