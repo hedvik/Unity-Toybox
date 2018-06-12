69ef29ee29297afc94b85b0cf7dc2cf0d837f178
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2018-06-12 12:27:50 +0200

    Added a realtime light to the baked lighting scene as well as a sphere using reflection probes

--- ExperimentalScenes/Assets/Scripts/ObjectRotatorComponent.cs
+++ ExperimentalScenes/Assets/Scripts/ObjectRotatorComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
 
+[RequireComponent(typeof(GameObjectEntity))]
 public class ObjectRotatorComponent : MonoBehaviour {
     public float speed;
     public Vector3 axis;
