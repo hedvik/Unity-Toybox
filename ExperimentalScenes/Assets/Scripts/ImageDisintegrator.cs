9b17224e3e76e3c1078e4a2a152b29db9a4e63b0
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2018-06-14 21:21:30 +0200

    Added a Scene testing image disintegration using particle systems

---
+++ ExperimentalScenes/Assets/Scripts/ImageDisintegrator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageDisintegrator : MonoBehaviour {
+    public Texture2D image;
+    public ImageParticlePlacer particlePlacer;
+
+    private void Start()
+    {
+        var colorData = image.GetPixels();
+        particlePlacer.PlaceParticles(colorData, image.width, image.height);
+    }
+}
---
+++ ExperimentalScenes/Assets/Scripts/ImageParticlePlacer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageParticlePlacer : MonoBehaviour {
+    public float stepSize = 1;
+    public Vector2 startPos = new Vector2(0, 0);
+    public float particleSize = 0.1f;
+
+    private ParticleSystem particleSys;
+
+    private void Start()
+    {
+        particleSys = GetComponent<ParticleSystem>();
+        var sys = particleSys.main;
+        sys.startSize = new ParticleSystem.MinMaxCurve(particleSize);
+    }
+
+    public void PlaceParticles(Color[] colors, int width, int height)
+    {
+        particleSys.Play();
+
+        var x = 0;
+        var y = 0;
+        for (var i = 0; i < colors.Length; i++)
+        {
+            if (x == width)
+            {
+                y++;
+                x = 0;
+            }
+
+            var emitParams = new ParticleSystem.EmitParams();
+            emitParams.position = new Vector3(x * stepSize, y * stepSize, 0);
+            emitParams.velocity = new Vector3(0, 0, 0);
+            emitParams.startColor = colors[i];
+            particleSys.Emit(emitParams, 1);
+
+            x++;
+        }
+    }
+}
