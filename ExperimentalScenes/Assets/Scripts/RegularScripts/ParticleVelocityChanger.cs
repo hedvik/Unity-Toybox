50d4847f8c6774f800338bc004302b32bd17fbae
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2018-06-15 09:19:39 +0200

    Improved disintegration effect

---
+++ ExperimentalScenes/Assets/Scripts/RegularScripts/ParticleVelocityChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleVelocityChanger : MonoBehaviour {
+    public float velocityStrength = 1;
+
+    private ParticleSystem particleSys;
+    private List<ParticleSystem.Particle> enteredParticles = new List<ParticleSystem.Particle>();
+
+    private void Start()
+    {
+        particleSys = GetComponent<ParticleSystem>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnParticleTrigger()
+    {
+        int numEnter = particleSys.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enteredParticles);
+        for(int i = 0; i < numEnter; i++)
+        {
+            ParticleSystem.Particle particle = enteredParticles[i];
+            particle.velocity = new Vector3(Random.Range(5, 10), Random.Range(-10, 10), 0).normalized * velocityStrength;
+            enteredParticles[i] = particle;
+        }
+        particleSys.SetTriggerParticles(ParticleSystemTriggerEventType.Enter, enteredParticles);
+    }
+}
---
+++ ExperimentalScenes/Assets/Scripts/RegularScripts/PositionLerp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionLerp : MonoBehaviour {
+    public Vector3 destination;
+    public float speed;
+
+    private Vector3 originalPosition = new Vector3();
+    private float lerpTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+        originalPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (lerpTimer > 1)
+            return;
+
+        lerpTimer += Time.deltaTime * speed;
+        transform.position = Vector3.Lerp(originalPosition, destination, lerpTimer);
+	}
+}
